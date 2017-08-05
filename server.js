3b2b0a68562ef0a232f4b9bd305250227d957596
muralibaskar <muralibaskar13@gmail.com>
2017-08-05 21:55:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one' , function (req, res) {
+    res.send('Article one rquested and will be serve here');
+});
+
+app.get('/article-two' , function (req, res) {
+    res.send('Article two rquested and will be serve here');
+});
+
+app.get('/article-three' , function (req, res) {
+    res.send('Article three rquested and will be serve here');
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
